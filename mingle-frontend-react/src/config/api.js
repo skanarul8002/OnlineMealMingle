fa57916698454f5a823a6adfaf9d39a8391f369a
Sk Anarul <112712827+skanarul8002@users.noreply.github.com>
2025-10-14 22:43:36 +0530

    Update api.js
    API url

--- mingle-frontend-react/src/config/api.js
+++ mingle-frontend-react/src/config/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export const API_URL = "http://localhost:5454";
+//export const API_URL = "http://localhost:5454";
 // export const API_URL = "https://mealmingle-4dkb.onrender.com";
-
+export const API_URL = "https://onlinemealmingle.onrender.com";
 export const api = axios.create({
   baseURL: API_URL, 
   headers: {
